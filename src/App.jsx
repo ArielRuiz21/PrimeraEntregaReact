15eda9b2a0a2fffdfd69fa1c3c2d2c5b5ef70e05
Alejandro Ruiz Diaz <alee_ruizdiaz@hotmail.com>
2024-11-06 14:30:20 -0300

    Primera Entrega React

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
+import NavBar from "./components/Navbar/Navbar"
+
+
+function App() {
+  
+
+  return (
+    <>
+    <NavBar />
+    <hr />
+    <ItemListContainer  greeting="Bienvenido a la tienda de Golosinas"/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,5 @@
+ export default function ItemListContainer(props){
+    return(
+        <h2 style={{color:'blue'}}>{props.greeting}</h2>
+    )
+}
